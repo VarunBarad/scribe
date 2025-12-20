ca68ccedae89c6341635411f951a84dd89432613
Varun Barad <varun@varunbarad.com>
2025-12-20 16:45:34 +0530

    Update setup script to install dependencies required for building foo2zjs and also the patch needed to be made to compile it correctly.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install CUPS server
-sudo apt-get install -y cups git
+sudo apt-get install -y build-essential tix foomatic-filters groff dc cups git
 sudo usermod -a -G lpadmin varun
 sudo cupsctl --remote-any
 sudo systemctl restart cups
@@ -13,12 +13,15 @@ sudo systemctl restart cups
 # Try accessing web-interface on http://<pi-ip-address>:631
 
 # Install HP-1020 specific drivers
+curl https://raw.githubusercontent.com/VarunBarad/scribe/refs/heads/main/foo2zjs.patch > /home/varun/foo2zjs.patch
 git clone https://github.com/koenkooi/foo2zjs.git
 cd foo2zjs
+git apply /home/varun/foo2zjs.patch
+make clean
 make
 wget -O sihp1020.tar.gz https://www.quirinux.org/printers/sihp1020.tar.gz
 gunzip < sihp1020.tar.gz | tar -xvf -
-sudo apt-get remove system-config-printer-udev
+sudo apt-get remove -y system-config-printer-udev
 sudo make install
 sudo make install-hotplug
 sudo make cups
